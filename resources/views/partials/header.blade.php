b5d7bce41b5a448ce98705e6015d022aec9afc4d
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-04-27 16:23:05 +0200

    inseriti i link nella navbar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,14 +3,16 @@
       <img src="{{asset('images/dc-logo.png')}}" alt="Logo DC Comics" />
       <nav>
         <ul>
-          {{-- <li
-            v-for="(link, index) in links"
-            :key="link.text"
-            :class="{ active: index == activeIndex }"
-            @click.stop.prevent="activeIndex = index"
-          >
-            <a href="link.href">{{ link.text }}</a>
-          </li> --}}
+          <li><a href="">characters</a></li>
+          <li><a href="">comics</a></li>
+          <li><a href="">movies</a></li>
+          <li><a href="">tv</a></li>
+          <li><a href="">games</a></li>
+          <li><a href="">collectibles</a></li>
+          <li><a href="">videos</a></li>
+          <li><a href="">fans</a></li>
+          <li><a href="">news</a></li>
+          <li><a href="">shop</a></li>
         </ul>
       </nav>
     </div>
