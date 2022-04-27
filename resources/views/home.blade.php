0b38ffc2b832af9f3b5988baac1c21e3e15dc5f1
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-04-27 15:32:23 +0200

    jumbotron

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,14 @@
 @include('template.layout')
+<main>
+    <div id="jumbotron"></div>
+    <div class="container">
+      <div id="etichetta">CURRENT SERIES</div>
+      {{-- <CardSerie
+        v-for="item in series"
+        :key="item.thumb"
+        :img="item.thumb"
+        :serie="item.series"
+      /> --}}
+    </div>
+    <button>LOAD MORE</button>
+  </main>
